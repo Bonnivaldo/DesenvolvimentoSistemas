3feba73101faf71b4795d7dc47238433f68130f2
Aluno <cleidson.bononi@gmail.com>
2020-03-05 22:38:49 -0300

    Desenvolvimento de telas de ocultar e desabilitar

--- Cleidson Bononi/AtividadeEntrega/src/atividadeentrega/Cadastro.java
+++ Cleidson Bononi/AtividadeEntrega/src/atividadeentrega/Cadastro.java
@@ -7,9 +7,9 @@ import javax.swing.text.*;
 
 public class Cadastro extends JFrame {
     
-    JLabel jlbtitulo,jlbnome, jlbidade, jlbtel, jlbcelular, jlbcpf, jlbendereco, jlbcep, jlbestado;
-    JTextField jtfnome, jtfidade, jtfendereco, jtfestado;
-    JFormattedTextField jftftel, jftfcelular, jftfcpf, jftfcep;
+    JLabel jltitulo,jlnome, jlidade, jltel, jlcelular, jlcpf, jlendereco, jlcep, jlestado;
+    JTextField jtnome, jtidade, jtendereco, jtestado;
+    JFormattedTextField jftel, jfcelular, jfcpf, jfcep;
     MaskFormatter mftel, mfcelular, mfcpf, mfcep;
     JButton jb1,jb2,jb3;
    
@@ -19,65 +19,65 @@ public class Cadastro extends JFrame {
         setLayout(null);
         
         
-        jlbtitulo = new JLabel("Cadastro de Clientes");
-        jlbnome = new JLabel("Nome:");
-        jlbidade = new JLabel("Idade:");
-        jlbtel = new JLabel("Telefone:");
-        jlbcelular = new JLabel("Celular:");
-        jlbcpf = new JLabel("CPF:");
-        jlbendereco = new JLabel("Endereço:");
-        jlbcep = new JLabel("CEP:");
-        jlbestado = new JLabel("Estado:");
+        jltitulo = new JLabel("Cadastro de Clientes");
+        jlnome = new JLabel("Nome:");
+        jlidade = new JLabel("Idade:");
+        jltel = new JLabel("Telefone:");
+        jlcelular = new JLabel("Celular:");
+        jlcpf = new JLabel("CPF:");
+        jlendereco = new JLabel("Endereço:");
+        jlcep = new JLabel("CEP:");
+        jlestado = new JLabel("Estado:");
         
-        jlbtitulo.setFont(new Font("Arial",Font.BOLD,16));
-        jlbnome.setFont(new Font("Arial",Font.BOLD,14));
-        jlbidade.setFont(new Font("Arial",Font.BOLD,14));
-        jlbtel.setFont(new Font("Arial",Font.BOLD,14));
-        jlbcelular.setFont(new Font("Arial",Font.BOLD,14));
-        jlbcpf.setFont(new Font("Arial",Font.BOLD,14));
-        jlbendereco.setFont(new Font("Arial",Font.BOLD,14));
-        jlbcep.setFont(new Font("Arial",Font.BOLD,14));
-        jlbestado.setFont(new Font("Arial",Font.BOLD,14));
+        jltitulo.setFont(new Font("Arial",Font.BOLD,16));
+        jlnome.setFont(new Font("Arial",Font.BOLD,14));
+        jlidade.setFont(new Font("Arial",Font.BOLD,14));
+        jltel.setFont(new Font("Arial",Font.BOLD,14));
+        jlcelular.setFont(new Font("Arial",Font.BOLD,14));
+        jlcpf.setFont(new Font("Arial",Font.BOLD,14));
+        jlendereco.setFont(new Font("Arial",Font.BOLD,14));
+        jlcep.setFont(new Font("Arial",Font.BOLD,14));
+        jlestado.setFont(new Font("Arial",Font.BOLD,14));
         
-        jlbtitulo.setForeground(Color.blue);
+        jltitulo.setForeground(Color.blue);
         
-        jlbtitulo.setBounds(125,5,250,20);
-        jlbnome.setBounds(50,50,60,20);
-        jlbidade.setBounds(50,80,60,20);
-        jlbtel.setBounds(50,110,80,20);
-        jlbcelular.setBounds(50,140,80,20);
-        jlbcpf.setBounds(50,170,60,20);
-        jlbendereco.setBounds(50,200,80,20);
-        jlbcep.setBounds(50,230,60,20);
-        jlbestado.setBounds(50,260,60,20);
+        jltitulo.setBounds(125,5,250,20);
+        jlnome.setBounds(50,50,60,20);
+        jlidade.setBounds(50,80,60,20);
+        jltel.setBounds(50,110,80,20);
+        jlcelular.setBounds(50,140,80,20);
+        jlcpf.setBounds(50,170,60,20);
+        jlendereco.setBounds(50,200,80,20);
+        jlcep.setBounds(50,230,60,20);
+        jlestado.setBounds(50,260,60,20);
                 
-        tela.add(jlbtitulo);
-        tela.add(jlbnome);
-        tela.add(jlbidade);
-        tela.add(jlbtel);
-        tela.add(jlbcelular);
-        tela.add(jlbcpf);
-        tela.add(jlbendereco);
-        tela.add(jlbcep);
-        tela.add(jlbestado);
+        tela.add(jltitulo);
+        tela.add(jlnome);
+        tela.add(jlidade);
+        tela.add(jltel);
+        tela.add(jlcelular);
+        tela.add(jlcpf);
+        tela.add(jlendereco);
+        tela.add(jlcep);
+        tela.add(jlestado);
         
         
         
-        jtfnome = new JTextField();        
-        jtfidade = new JTextField();
-        jtfestado = new JTextField();
-        jtfendereco = new JTextField();
+        jtnome = new JTextField();        
+        jtidade = new JTextField();
+        jtestado = new JTextField();
+        jtendereco = new JTextField();
         
-        jtfnome.setBounds(150,50,250,20);
-        jtfidade.setBounds(150,80,25,20);
-        jtfendereco.setBounds(150,200,250,20);
-        jtfestado.setBounds(150,260,20,20);
+        jtnome.setBounds(150,50,250,20);
+        jtidade.setBounds(150,80,25,20);
+        jtendereco.setBounds(150,200,250,20);
+        jtestado.setBounds(150,260,20,20);
         
         
-        tela.add(jtfnome);
-        tela.add(jtfidade);
-        tela.add(jtfestado);
-        tela.add(jtfendereco);
+        tela.add(jtnome);
+        tela.add(jtidade);
+        tela.add(jtestado);
+        tela.add(jtendereco);
         
         
         
@@ -95,31 +95,33 @@ public class Cadastro extends JFrame {
         }
         catch(ParseException excp){}
         
-        jftfcelular = new JFormattedTextField(mfcelular);
-        jftfcep = new JFormattedTextField(mfcep);
-        jftfcpf = new JFormattedTextField(mfcpf);
-        jftftel = new JFormattedTextField(mftel);
+        jfcelular = new JFormattedTextField(mfcelular);
+        jfcep = new JFormattedTextField(mfcep);
+        jfcpf = new JFormattedTextField(mfcpf);
+        jftel = new JFormattedTextField(mftel);
         
-        jftftel.setBounds(150,110,90,20);
-        jftfcelular.setBounds(150,140,95,20);
-        jftfcpf.setBounds(150,170,95,20);
-        jftfcep.setBounds(150,230,70,20);
+        jftel.setBounds(150,110,90,20);
+        jfcelular.setBounds(150,140,95,20);
+        jfcpf.setBounds(150,170,95,20);
+        jfcep.setBounds(150,230,70,20);
         
-        tela.add(jftftel);
-        tela.add(jftfcpf);
-        tela.add(jftfcep);
-        tela.add(jftfcelular);
+        tela.add(jftel);
+        tela.add(jfcpf);
+        tela.add(jfcep);
+        tela.add(jfcelular);
         
         
         
-        ImageIcon icone1 = new ImageIcon("sign-in.png"); 
+        ImageIcon icone1 = new ImageIcon("skip_backward_16x16.gif"); 
         setIconImage(icone1.getImage());
-        ImageIcon icone2 = new ImageIcon("sign-out.png"); 
+        ImageIcon icone2 = new ImageIcon("skip_forward_16x16.gif"); 
         setIconImage(icone2.getImage());
+        ImageIcon icone3 = new ImageIcon("user8_(edit)_16x16.gif"); 
+        setIconImage(icone3.getImage());
         
-        jb1= new JButton("Voltar",icone2);
+        jb1= new JButton("Voltar",icone1);
         jb2= new JButton("Cadastrar");
-        jb3= new JButton("Próximo",icone1);
+        jb3= new JButton("Próximo",icone2);
         
         jb1.setBounds(50,300,120,20);
         jb2.setBounds(200,300,120,20);
